35515ad33f711f6dcea995cadb00b22abca257e3
chspower1 <chspower1@gmail.com>
2022-09-17 01:42:17 +0900

    finish Drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { render } from "react-dom";
 
+// Styled - Components
 const Wrapper = styled.div`
     height: 100vh;
     width: 100vw;
-    display: flex;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
     justify-content: center;
     align-items: center;
 `;
 const Container = styled.div<{ bgColor?: string }>`
+    position: relative;
     background: linear-gradient(140deg, #fcfcfc, ${(props) => props.bgColor});
     width: 250px;
     height: 250px;
@@ -22,14 +25,30 @@ const Container = styled.div<{ bgColor?: string }>`
         background-color: ${(props) => props.bgColor};
     }
 `;
-const Box = styled(motion.div)`
+const Title = styled.h1`
+    color: white;
+    z-index: 1000;
+    position: absolute;
+    bottom: 10px;
+    left: 10px;
+    font-weight: 800;
+`;
+const Box = styled(motion.div)<{ isDrag?: boolean }>`
     width: 100px;
     height: 100px;
     border-radius: 20px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
     background-color: white;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    overflow: hidden;
+    display: ${(props) => (props.isDrag ? "flex" : "grid")};
+    justify-content: center;
+    grid-template-columns: ${(props) => (props.isDrag ? "none" : "repeat(2, 1fr)")};
+`;
+const DragBox = styled(Box)`
+    width: 50px;
+    height: 50px;
+    place-self: center;
+    background-color: #00b894;
 `;
 const Circle = styled(motion.div)`
     z-index: 1000;
@@ -38,12 +57,22 @@ const Circle = styled(motion.div)`
     border-radius: 18px;
     place-self: center;
 `;
+
+// Variants
 const myVars1 = {
     start: { scale: 0 },
     end: {
         scale: 1,
         rotateZ: 180,
         transition: { type: "spring", stiffness: 260, damping: 20 },
+    },
+    hover: {
+        scale: 1.1,
+        rotateZ: 90,
+    },
+    tap: {
+        scale: 0.7,
+        borderRadius: "50px",
     },
 };
 const myVars2 = {
@@ -65,19 +94,40 @@ const circleVariants = {
         y: 0,
     },
 };
+
 function App() {
+    const constranisRef = useRef(null);
     return (
         <Wrapper>
-            <button onClick={() => window.location.reload()}>새로고침</button>
+            <button style={{ position: "absolute" }} onClick={() => window.location.reload()}>
+                새로고침
+            </button>
             <Container bgColor="#74b9ff">
+                <Title>Animation</Title>
                 <Box variants={myVars1} initial="start" animate="end" />
             </Container>
             <Container bgColor="#fd79a8">
+                <Title>Variants</Title>
                 <Box variants={myVars2} initial="start" animate="end">
                     <Circle className="circle" variants={circleVariants} />
                     <Circle className="circle" variants={circleVariants} />
                     <Circle className="circle" variants={circleVariants} />
                     <Circle className="circle" variants={circleVariants} />
+                </Box>
+            </Container>
+            <Container bgColor="#00b894">
+                <Title>Gestures</Title>
+                <Box variants={myVars1} whileHover="hover" whileTap="tap" />
+            </Container>
+            <Container bgColor="#00b894">
+                <Title>Drag</Title>
+                <Box variants={myVars1} isDrag={true} ref={constranisRef}>
+                    {/* transition을 주려면 rgb(숫자)값으로 줘야함 */}
+                    <DragBox
+                        drag
+                        dragConstraints={constranisRef}
+                        whileDrag={{ backgroundColor: "rgb(85, 239, 196)" }}
+                    />
                 </Box>
             </Container>
         </Wrapper>

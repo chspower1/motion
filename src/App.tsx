a0cc198ed3d8a7b595a25f6317d1c81e8287a6f8
chspower1 <chspower1@gmail.com>
2022-09-20 00:49:26 +0900

    path animation 완료

--- src/App.tsx
+++ src/App.tsx
@@ -98,6 +98,13 @@ const circleVariants = {
         y: 0,
     },
 };
+const pathVar = {
+    start: { pathLength: 0, fill: "rgba(255,255,255,0)" },
+    end: {
+        pathLength: 1,
+        fill: "rgba(255,255,255,1)",
+    },
+};
 
 function App() {
     const constranisRef = useRef(null);
@@ -164,10 +171,16 @@ function App() {
                 <Box style={{ scale: ScrollScale }} />
             </Container>
             <Container bgColor="rgb(116, 185, 255)">
-                <Title>Scroll</Title>
+                <Title>Path</Title>
                 <Svg viewBox="0 0 576 512">
                     <motion.path
-                        fill="transparent"
+                        variants={pathVar}
+                        initial={"start"}
+                        animate={"end"}
+                        transition={{
+                            default: { duration: 3 },
+                            fill: { duration: 2, delay: 1 },
+                        }}
                         stroke="white"
                         strokeWidth="10"
                         d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"

8009d8620b05078269877bdd160a7c1bdde8bbe7
chspower1 <chspower1@gmail.com>
2022-09-20 20:19:17 +0900

    Issue, next 클릭 후 prev클릭시 방향전환 x

--- src/App.tsx
+++ src/App.tsx
@@ -44,6 +44,14 @@ const RedirectBtn = styled.button`
 
     cursor: pointer;
 `;
+const NextBtn = styled(RedirectBtn)`
+    right: 0px;
+    bottom: 50%;
+    transform: translateY(50%);
+`;
+const PrevBtn = styled(NextBtn)`
+    left: 0px;
+`;
 const ContentContainer = styled(motion.div)<{ bgColor?: string }>`
     position: relative;
     background: linear-gradient(140deg, rgb(252, 252, 252), ${(props) => props.bgColor});
@@ -67,6 +75,7 @@ const ContentTitle = styled.h1`
     font-weight: 800;
 `;
 const Box = styled(motion.div)<{ isDrag?: boolean }>`
+    position: relative;
     width: 100px;
     height: 100px;
     border-radius: 20px;
@@ -82,6 +91,15 @@ const DragBox = styled(Box)`
     height: 50px;
     place-self: center;
     background-color: rgb(255, 118, 117);
+`;
+const SlideNumber = styled.div`
+    position: absolute;
+    font-size: 26px;
+    font-weight: 800;
+    color: #fab1a0;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 `;
 const Circle = styled(motion.div)`
     z-index: 1000;
@@ -138,22 +156,21 @@ const pathVar = {
         fill: "rgba(255,255,255,1)",
     },
 };
-
 const slideVar = {
-    invisible: {
+    entry: (next: boolean) => ({
+        x: next ? 100 : -100,
         scale: 0,
-        x: 100,
-    },
-    visible: {
-        scale: 1,
-        x: 0,
+    }),
+    exit: (next: boolean) => ({
+        x: next ? -100 : 100,
+        scale: 0,
         transition: {
             duration: 1,
         },
-    },
-    leaving: {
-        scale: 0,
-        x: -100,
+    }),
+    center: {
+        scale: 1,
+        x: 0,
         transition: {
             duration: 1,
         },
@@ -163,6 +180,7 @@ const slideVar = {
 function App() {
     const [refresh, setRefresh] = useState([0, 0, 0]);
     const [showing, setShowing] = useState(0);
+    const [next, setNext] = useState<boolean>();
     const constranisRef = useRef(null);
     const scrollRef = useRef(null);
     const x = useMotionValue(0);
@@ -184,7 +202,11 @@ function App() {
             return result;
         });
     };
-
+    const onClickArrowBtn = (next: boolean) => {
+        setNext(next);
+        setShowing((cur) => (next ? cur + 1 : cur - 1));
+        console.log(next);
+    };
     useEffect(() => {}, []);
     return (
         <Wrapper>
@@ -298,37 +320,43 @@ function App() {
                     </Svg>
                 </ContentContainer>
                 {/* Slide */}
-                <ContentContainer bgColor="rgb(116, 185, 255)">
+                <ContentContainer bgColor="rgb(250, 177, 160)">
                     <ContentTitle>Slide</ContentTitle>
-                    <AnimatePresence>
-                        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) =>
-                            showing === item ? (
-                                <Box
-                                    style={{ position: "absolute" }}
-                                    key={item}
-                                    variants={slideVar}
-                                    initial="invisible"
-                                    animate="visible"
-                                    exit="leaving"
-                                >
-                                    <div style={{ placeSelf: "center", width: "100%" }}>{item}</div>
-                                </Box>
-                            ) : null
-                        )}
+                    <AnimatePresence custom={next}>
+                        <Box
+                            custom={next}
+                            style={{ position: "absolute" }}
+                            variants={slideVar}
+                            initial="entry"
+                            animate="center"
+                            exit="exit"
+                            key={showing}
+                        >
+                            <SlideNumber>{showing}</SlideNumber>
+                        </Box>
                     </AnimatePresence>
-                    <RedirectBtn
-                        onClick={() => (showing === 10 ? null : setShowing((cur) => cur + 1))}
-                    >
+                    <NextBtn onClick={() => onClickArrowBtn(true)}>
                         <svg
                             width={"100%"}
                             height={"100%"}
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512"
                             fill={"white"}
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512"
                         >
-                            <path d="M48.5 224H40c-13.3 0-24-10.7-24-24V72c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2L98.6 96.6c87.6-86.5 228.7-86.2 315.8 1c87.5 87.5 87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3c-62.2-62.2-162.7-62.5-225.3-1L185 183c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8H48.5z" />
+                            <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
                         </svg>
-                    </RedirectBtn>
+                    </NextBtn>
+                    <PrevBtn onClick={() => onClickArrowBtn(false)}>
+                        <svg
+                            width={"100%"}
+                            height={"100%"}
+                            fill={"white"}
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512"
+                        >
+                            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+                        </svg>
+                    </PrevBtn>
                 </ContentContainer>
             </SampleContainer>
         </Wrapper>

03af8e8e2b2d2248b1a1edc521058edf1d4af64a
chspower1 <chspower1@gmail.com>
2022-09-20 14:17:13 +0900

    refresh 기능 수정

--- src/App.tsx
+++ src/App.tsx
@@ -132,7 +132,7 @@ const pathVar = {
 };
 
 function App() {
-    const [refresh, setRefresh] = useState([0, 0]);
+    const [refresh, setRefresh] = useState([0, 0, 0]);
     const constranisRef = useRef(null);
     const scrollRef = useRef(null);
     const x = useMotionValue(0);
@@ -147,22 +147,15 @@ function App() {
     const { scrollYProgress } = useScroll();
     const ScrollScale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
 
-    const onClickRefresh = (num: number) => {
-        if (num === 0) {
-            setRefresh((cur) => {
-                return [cur[0] + 1, cur[1]];
-            });
-        } else if (num === 1) {
-            setRefresh((cur) => {
-                return [cur[0], cur[1] + 1];
-            });
-        }
+    const onClickRefresh = (index: number) => {
+        setRefresh((cur) => {
+            const result = [...cur];
+            result[index] = result[index] + 1;
+            return result;
+        });
     };
 
-    useEffect(() => {
-        // x.onChange(() => console.log(x.get()));
-        y.onChange(() => console.log(y.get()));
-    }, []);
+    useEffect(() => {}, []);
     return (
         <Wrapper>
             <Title>Motion Sample</Title>
@@ -185,7 +178,7 @@ function App() {
                     </RedirectBtn>
                 </ContentContainer>
                 {/* Variants */}
-                <ContentContainer bgColor="rgb(253, 121, 168)" key={refresh[1]}>
+                <ContentContainer bgColor="rgb(253, 121, 168)">
                     <ContentTitle>Variants</ContentTitle>
                     <RedirectBtn onClick={() => onClickRefresh(1)}>
                         <svg
@@ -198,7 +191,7 @@ function App() {
                             <path d="M48.5 224H40c-13.3 0-24-10.7-24-24V72c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2L98.6 96.6c87.6-86.5 228.7-86.2 315.8 1c87.5 87.5 87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3c-62.2-62.2-162.7-62.5-225.3-1L185 183c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8H48.5z" />
                         </svg>
                     </RedirectBtn>
-                    <Box variants={myVars2} initial="start" animate="end">
+                    <Box variants={myVars2} initial="start" animate="end" key={refresh[1]}>
                         <Circle className="circle" variants={circleVariants} />
                         <Circle className="circle" variants={circleVariants} />
                         <Circle className="circle" variants={circleVariants} />
@@ -247,7 +240,18 @@ function App() {
                     <Box style={{ scale: ScrollScale }} />
                 </ContentContainer>
                 {/* Path */}
-                <ContentContainer bgColor="rgb(116, 185, 255)">
+                <ContentContainer bgColor="rgb(116, 185, 255)" key={refresh[2]}>
+                    <RedirectBtn onClick={() => onClickRefresh(2)}>
+                        <svg
+                            width={"100%"}
+                            height={"100%"}
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                            fill={"white"}
+                        >
+                            <path d="M48.5 224H40c-13.3 0-24-10.7-24-24V72c0-9.7 5.8-18.5 14.8-22.2s19.3-1.7 26.2 5.2L98.6 96.6c87.6-86.5 228.7-86.2 315.8 1c87.5 87.5 87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3c-62.2-62.2-162.7-62.5-225.3-1L185 183c6.9 6.9 8.9 17.2 5.2 26.2s-12.5 14.8-22.2 14.8H48.5z" />
+                        </svg>
+                    </RedirectBtn>
                     <ContentTitle>Path</ContentTitle>
                     <Svg viewBox="0 0 576 512">
                         <motion.path
@@ -255,8 +259,8 @@ function App() {
                             initial={"start"}
                             animate={"end"}
                             transition={{
-                                default: { duration: 3 },
-                                fill: { duration: 2, delay: 1 },
+                                default: { duration: 2 },
+                                fill: { duration: 1.5, delay: 0.5 },
                             }}
                             stroke="white"
                             strokeWidth="10"
